3b00e1491595b67c0b813e58d7da496a374d5a6e
Yuvraj049 <yvc3094@gmail.com>
2023-09-09 15:03:21 +0530

    changes on Login Signup page
    remember me and forget checkboxes created, js file and made changes in username and password div to make input fields inclick event and save info

---
+++ Frontend_and_UI_Design/Login Signup Page/js/index.js
@@ -0,0 +1,27 @@
+function convertDivtoInput(divid,divuserid,defaultName) {
+    const divContainer = document.getElementById(divid);
+    divHTML = divContainer.innerHTML;
+    const inputElement = document.createElement('input');
+    inputElement.className = 'divtoinput'; // to give CSS
+    if(typeof divContainer.value == 'undefined'){
+        inputElement.value ="";
+    }else{
+        inputElement.value = divContainer.value;
+    }
+    inputElement.addEventListener('blur', () => {
+        divContainer.innerHTML = divHTML;
+        divContainer.value = inputElement.value;
+        const divuser=document.getElementById(divuserid);
+        if(inputElement.value==""){
+            divuser.innerText = defaultName;
+        }else{
+            divuser.innerText = inputElement.value;
+        }
+        divContainer.className = divid;
+        divContainer.style.display = 'flex';// for making non-none in display for username
+        inputElement.remove();
+    });
+    divContainer.style.display = 'none';
+    divContainer.parentNode.insertBefore(inputElement, divContainer.nextSibling);
+    inputElement.focus();
+}
