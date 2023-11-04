6342e100fff2d5ada04975ac906c20966a405a22
Vansh N Joshi <vanshj008@gmail.com>
2023-11-04 11:54:12 +0530

    Learnt how to grab data from radio button

--- DeployML3/mlmodel/views.py
+++ DeployML3/mlmodel/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from joblib import load
 diab_model = load('./SavedModels/diabetes_model.joblib')
-heart_model = load('./SavedModels/heart_model.joblib')
+# heart_model = load('./SavedModels/heart_model.joblib')
 
 def home(request):
     return HttpResponse('Helo')
@@ -16,6 +16,11 @@ def predictor_diab(request):
 def formInfo_diab(request):
 
     HighBP = request.GET['HighBP']
+    if HighBP=='No':
+        HighBP=1
+    else:
+        HighBP=0
+    print(HighBP)
     HighChol = request.GET['HighChol']
     BMI = request.GET['BMI']
     Stroke = request.GET['Stroke']
@@ -23,7 +28,7 @@ def formInfo_diab(request):
     GenHlth = request.GET['GenHlth']
     Age = request.GET['Age']
     y_pred = diab_model.predict_proba([[HighBP,HighChol,BMI,Stroke,HeartDiseaseorAttack,GenHlth,Age]])
-    y_pred = y_pred
+    y_pred = y_pred*100
     return render(request,'mlmodel/result_diab.html',{'data':y_pred[0][1]})
 
 
